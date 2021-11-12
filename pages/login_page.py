54aaa3538517f9752c25e2ee19a23f079ce79a4d
Andrew <p55-cd53@yandex.ru>
2021-11-12 12:33:19 +0300

    Step 8 completed, login_page.py added

---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.common.by import By
+
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert 'login' in self.browser.current_url
+
+    def should_be_login_form(self):
+        assert self.is_element_present(By.CSS_SELECTOR, 'form#login_form')
+
+    def should_be_register_form(self):
+        assert self.is_element_present(By.CSS_SELECTOR, 'form#register_form')
